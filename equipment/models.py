f721b15e8a240cf6b6b79fc4497e6bb90b93df3f
Pampamlela <pampamlela@gmail.com>
2026-02-17 18:10:34 +0100

    Add equipment models (Camera, Lens)

--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Camera(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+          on_delete=models.CASCADE, 
+          related_name="cameras")
+    model = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.model}"
+    
+class Lens(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+          on_delete=models.CASCADE, 
+          related_name="lenses"
+    )
+    camera = models.ForeignKey(
+        Camera,
+          on_delete=models.CASCADE, 
+          related_name="lenses"
+    )
+    model = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.model}"
